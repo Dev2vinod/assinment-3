4030affd9d0c162d80029e8b0b74315bbd8e04f9
VINOD SINGH <vinodsinghfun@gmail.com>
2024-01-03 08:04:23 +0530

    assinment-3

---
+++ app.js
@@ -0,0 +1,29 @@
+//Q1
+ var age = " i am 25 year old"
+//  alert(age);
+
+//  sweetAlert(age);
+
+//Q2
+var num = 14;
+//  alert("You have a visited "+num +" times")
+
+ //Q3
+
+ var birthYear = 2003;
+ 
+//  alert("My birth year is " + birthYear + " \nData type of my declared varible is number");
+
+ //Q4 
+ var name = "haider khan";
+ var title ="T-shirt";
+ var quantity = 5;
+
+//  alert( name +" odered "+ quantity +title+" on xyz Clothing site")
+
+
+
+
+
+
+
